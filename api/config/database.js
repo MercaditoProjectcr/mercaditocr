017360d900c8d659edddec47fa9ba7b31c048d6e
Jose Chavarria <joserafaelcascante@gmail.com>
2020-04-04 14:29:51 -0600

    add test endpoint with crud function, in this case the api use a post collection

--- api/config/database.js
+++ api/config/database.js
@@ -3,8 +3,9 @@ import mongoose from "mongoose";
 class Connection {
   constructor() {
     const url =
-      process.env.MONGODB_URI || `mongodb://localhost:27017/TestDB`;
-    console.log("Establish new connection with url", url);
+      // process.env.MONGODB_URI || `mongodb://localhost:27017/TestDB`;
+      process.env.MONGODB_URI || `mongodb+srv://mercaditoUser:8fyETnGwivC1RQTb@cluster0-wxxtk.mongodb.net/mercaditocrdb?retryWrites=true&w=majority`;
+    console.log("Establish new connection with the DB");
     mongoose.Promise = global.Promise;
     mongoose.set("useNewUrlParser", true);
     mongoose.set("useFindAndModify", false);
