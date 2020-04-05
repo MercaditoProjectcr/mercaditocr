1a814bbb09c94a10ed50a05b0d679621687dfad6
Jose Chavarria <joserafaelcascante@gmail.com>
2020-04-04 21:39:08 -0600

    add endpoint user and all the functions of crud using the generic crud

--- api/src/models/Post.js
+++ api/src/models/Post.js
@@ -2,38 +2,39 @@ import mongoose, { Schema } from "mongoose";
 import uniqueValidator from "mongoose-unique-validator";
 import slugify from 'slugify';
 
+const dto = {
+  title: {
+    type: String,
+    required: true,
+  },
+  slug: String,
+  subtitle: {
+    type: String,
+    required: false,
+  },
+  description: {
+    type: String,
+    required: false,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  contacts: {
+    name: {
+      type: String,
+    required: false
+    },
+    num: {
+      type: Number,
+      required: false
+    }
+  }
+}
 class Post {
 
   initSchema() {
-    const schema = new Schema({
-      title: {
-        type: String,
-        required: true,
-      },
-      slug: String,
-      subtitle: {
-        type: String,
-        required: false,
-      },
-      description: {
-        type: String,
-        required: false,
-      },
-      content: {
-        type: String,
-        required: true,
-      },
-      contacts: {
-        name: {
-          type: String,
-        required: false
-        },
-        num: {
-          type: Number,
-          required: false
-        }
-      }
-    }, { timestamps: true });
+    const schema = new Schema(dto, { timestamps: true });
     schema.pre(
       "save",
       function(next) {
