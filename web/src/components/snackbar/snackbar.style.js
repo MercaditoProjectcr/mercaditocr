5a8010931b160d1d922193ef9bbee09e1582f28d
Rafa <rafa.sequeira93@gmail.com>
2020-04-10 20:34:38 -0600

    signup and signin minor fixes, added alerts with snackbar component, other minor fixes

---
+++ web/src/components/snackbar/snackbar.style.js
@@ -0,0 +1,121 @@
+const conatinerFluid = {
+  paddingRight: "15px",
+  paddingLeft: "15px",
+  marginRight: "auto",
+  marginLeft: "auto",
+  width: "100%"
+};
+const container = {
+  ...conatinerFluid,
+  "@media (min-width: 576px)": {
+    maxWidth: "540px"
+  },
+  "@media (min-width: 768px)": {
+    maxWidth: "720px"
+  },
+  "@media (min-width: 992px)": {
+    maxWidth: "960px"
+  },
+  "@media (min-width: 1200px)": {
+    maxWidth: "1140px"
+  }
+};
+
+const successBoxShadow = {
+  boxShadow:
+    "0 12px 20px -10px rgba(76, 175, 80, 0.28), 0 4px 20px 0px rgba(0, 0, 0, 0.12), 0 7px 8px -5px rgba(76, 175, 80, 0.2)"
+};
+const warningBoxShadow = {
+  boxShadow:
+    "0 12px 20px -10px rgba(255, 152, 0, 0.28), 0 4px 20px 0px rgba(0, 0, 0, 0.12), 0 7px 8px -5px rgba(255, 152, 0, 0.2)"
+};
+const dangerBoxShadow = {
+  boxShadow:
+    "0 12px 20px -10px rgba(244, 67, 54, 0.28), 0 4px 20px 0px rgba(0, 0, 0, 0.12), 0 7px 8px -5px rgba(244, 67, 54, 0.2)"
+};
+const infoBoxShadow = {
+  boxShadow:
+    "0 12px 20px -10px rgba(0, 188, 212, 0.28), 0 4px 20px 0px rgba(0, 0, 0, 0.12), 0 7px 8px -5px rgba(0, 188, 212, 0.2)"
+};
+const defaultFont = {
+  fontFamily: '"Roboto", "Helvetica", "Arial", sans-serif',
+  fontWeight: "300",
+  lineHeight: "1.5em"
+};
+
+const snackbarContentStyle = {
+  root: {
+    ...defaultFont,
+    position: "relative",
+    padding: "20px 15px",
+    lineHeight: "20px",
+    marginBottom: "20px",
+    fontSize: "14px",
+    backgroundColor: "white",
+    color: "#555555",
+    borderRadius: "0px",
+    maxWidth: "100%",
+    minWidth: "auto",
+    boxShadow:
+      "0 12px 20px -10px rgba(255, 255, 255, 0.28), 0 4px 20px 0px rgba(0, 0, 0, 0.12), 0 7px 8px -5px rgba(255, 255, 255, 0.2)"
+  },
+  info: {
+    backgroundColor: "#00d3ee",
+    color: "#ffffff",
+    ...infoBoxShadow
+  },
+  success: {
+    backgroundColor: "#5cb860",
+    color: "#ffffff",
+    ...successBoxShadow
+  },
+  warning: {
+    backgroundColor: "#ffa21a",
+    color: "#ffffff",
+    ...warningBoxShadow
+  },
+  danger: {
+    backgroundColor: "#f55a4e",
+    color: "#ffffff",
+    ...dangerBoxShadow
+  },
+  primary: {
+    backgroundColor: "#af2cc5",
+    color: "#ffffff"
+  },
+  message: {
+    padding: "0",
+    display: "block",
+    maxWidth: "89%",
+    "&,& *": {
+      letterSpacing: "normal"
+    }
+  },
+  close: {
+    width: "14px",
+    height: "14px"
+  },
+  iconButton: {
+    width: "24px",
+    height: "24px",
+    float: "right",
+    fontSize: "1.5rem",
+    fontWeight: "500",
+    lineHeight: "1",
+    position: "absolute",
+    right: "-4px",
+    top: "0",
+    padding: "0"
+  },
+  icon: {
+    display: "block",
+    float: "left",
+    marginRight: "1.071rem"
+  },
+  container: {
+    ...container,
+    position: "relative"
+  }
+};
+
+export default snackbarContentStyle;
