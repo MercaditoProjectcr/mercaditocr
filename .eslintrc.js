4fac901629cd01cc13f5bf866f589134629a18a0
Rafa <jchavarria@soin.co.cr>
2020-08-23 17:17:10 -0600

    test husky

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,29 @@
 module.exports = {
   env: {
+    browser: true,
     es2020: true,
     node: true,
   },
-  extends: ['airbnb-base', 'prettier'],
-  plugins: ['prettier'],
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'prettier',
+    'eslint:recommended',
+  ],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
   },
   parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
     ecmaVersion: 12,
     sourceType: 'module',
   },
+  plugins: ['react', 'prettier'],
   rules: {
-    'prettier/prettier': 'error',
+    'prettier/prettier': ['error', { singleQuote: true, parser: 'flow' }],
     'class-methods-use-this': 'off',
     'no-param-reassign': 'off',
     camelcase: 'off',
